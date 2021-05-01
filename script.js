7e1602d3c0b61a0b80587ad6ea277ccdf82544fc
prathamesh yerva <71729862+prathamesh88-github@users.noreply.github.com>
2021-05-01 21:54:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-        
-        function citySort() {
-            var table, rows, switching, i, x, y, shouldSwitch;
-            table = document.getElementById("show");
-            switching = true;
-            while (switching) {
-                switching = false;
-                rows = table.rows;
-                for (i = 1; i < (rows.length - 1); i++) {
-                    shouldSwitch = false;
-                    x = rows[i].getElementsByTagName("TD")[0];
-                    y = rows[i + 1].getElementsByTagName("TD")[0];
-                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                        shouldSwitch = true;
-                        break;
-                    }
-                }
-                if (shouldSwitch) {
-                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                    switching = true;
-                }
-            }
-        }
-        function populationSort() {
-            var table, rows, switching, i, x, y, shouldSwitch;
-            table = document.getElementById("show");
-            switching = true;
-            while (switching) {
-                switching = false;
-                rows = table.rows;
-                for (i = 1; i < (rows.length - 1); i++) {
-                    shouldSwitch = false;
-                    x = rows[i].getElementsByTagName("TD")[0];
-                    y = rows[i + 1].getElementsByTagName("TD")[0];
-                    if (Number(x.innerHTML) > Number(y.innerHTML)) {
-                        shouldSwitch = true;
-                        break;
-                    }
-                }
-                if (shouldSwitch) {
-                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                    switching = true;
-                }
-            }
-        }
-
-
         document.addEventListener('DOMContentLoaded', function() {
             const table = document.getElementById('show');
             const headers = table.querySelectorAll('th');
@@ -52,12 +5,12 @@
             var rows = tableBody.querySelectorAll('tr');
             const add_btn = document.getElementById('btn');
         
-            // Track sort directions
+       
             const directions = Array.from(headers).map(function(header) {
                 return '';
             });
         
-            // Transform the content of given cell in given column
+           
             const transform = function(index, content) {
                 // Get the data type of column
                 const type = headers[index].getAttribute('data-type');
@@ -71,10 +24,10 @@
             };
         
             const sortColumn = function(index) {
-                // Get the current direction
+               
                 const direction = directions[index] || 'asc';
         
-                // A factor based on the direction
+                
                 const multiplier = (direction === 'asc') ? 1 : -1;
         
                 const newRows = Array.from(rows);
@@ -94,7 +47,7 @@
                     }
                 });
         
-                // Remove old rows
+              
                 [].forEach.call(rows, function(row) {
                     tableBody.removeChild(row);
                 });
@@ -102,7 +55,7 @@
                 // Reverse the direction
                 directions[index] = direction === 'asc' ? 'desc' : 'asc';
         
-                // Append new row
+               
                 newRows.forEach(function(newRow) {
                     tableBody.appendChild(newRow);
                 });
